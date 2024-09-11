a738f33d9fc992879a0ec8ceb85c7f06463ea05f
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2024-09-11 20:49:25 +0800

    Refactor AuditableEntity class to initialize CreatedDate property with current UTC time and make CreatedBy and LastModifiedBy properties nullable

--- src/Core/TicketManagement.Domain/Common/AuditableEntity.cs
+++ src/Core/TicketManagement.Domain/Common/AuditableEntity.cs
@@ -2,8 +2,8 @@
 
 public class AuditableEntity
 {
-    public string CreatedBy { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public string LastModifiedBy { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public string? LastModifiedBy { get; set; }
     public DateTime? LastModifiedDate { get; set; }
 }
